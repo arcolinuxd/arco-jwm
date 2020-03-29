4157058805ea86f5ce678fc7e356775b5dd47cf1
erik <erik.dubois@gmail.com>
2020-03-29 09:57:11 +0200

    500

--- 500-software-AUR-repo-distro-specific-v1.sh
+++ 500-software-AUR-repo-distro-specific-v1.sh
@@ -24,13 +24,13 @@ sleep 3
 
 echo "AUR - DESKTOP SPECIFIC APPLICATIONS "
 
-sh AUR-DS/install-mugshot-v*.sh
+#sh AUR-DS/install-mugshot-v*.sh
 
-sh AUR-DS/install-menulibre-v*.sh
+#sh AUR-DS/install-menulibre-v*.sh
 
 sh AUR/install-yad-v*.sh
 
-sh AUR-DS/install-xfce4-panel-profiles-v*.sh
+#sh AUR-DS/install-xfce4-panel-profiles-v*.sh
 
 echo "################################################################"
 echo "####       Software from AUR-DS folder installed          ######"
